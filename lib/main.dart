c9bf54233b7da590348462e01303c3041da563fb
Qasim-afzaal <qasim.afzaal432@gmail.com>
2025-02-10 04:11:05 +0500

    call provider on main

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,44 @@
-import 'package:ai_agents_vertex_ai/home.dart';
+import 'package:ai_agents_vertex_ai/functionality/app_state.dart';
+import 'package:ai_agents_vertex_ai/functionality/routes,dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:provider/provider.dart';
 
-void main() {
-  runApp(const MyApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    // options: DefaultFirebaseOptions.currentPlatform,
+  );
+  runApp(
+    MultiProvider(
+      providers: [
+        ChangeNotifierProvider<AppState>(create: (_) => AppState()),
+      ],
+      child: const MyApp(),
+    ),
+  );
 }
 
-class MyApp extends StatefulWidget {
+class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
-  State<MyApp> createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
-  @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home:Home() ,
+    return ChangeNotifierProvider<ThemeNotifier>(
+      create: (_) => ThemeNotifier(),
+      builder: (context, child) {
+        return MaterialApp.router(
+          title: 'Vertex AI Agent & Flutter Demo',
+          debugShowCheckedModeBanner: false,
+          routerConfig: router,
+          theme: ThemeData(
+            colorScheme: ColorScheme.fromSeed(
+              brightness: context.watch<ThemeNotifier>().brightness,
+              seedColor: const Color.fromARGB(255, 171, 222, 244),
+            ),
+          ),
+        );
+      },
     );
   }
 }
